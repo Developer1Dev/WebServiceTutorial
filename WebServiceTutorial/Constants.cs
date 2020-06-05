a84dfa93fcf7739fa3b9cf3b8c962e4eb673031e
coding-sys <56183572+coding-sys@users.noreply.github.com>
2020-06-05 17:33:42 +0200

    Ajoutez des fichiers projet.

---
+++ WebServiceTutorial/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServiceTutorial 
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint= "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "f1c6f78c306b5adcb26ab65b86059d5f";
+    }
+}
---
+++ WebServiceTutorial/WeatherData.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace WebServiceTutorial
+{
+    public class WeatherData
+    {
+        [JsonProperty("name")]
+        public string Title { get; set; }
+
+        [JsonProperty("weather")]
+        public Weather[] Weather { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("visibility")]
+        public long Visibility { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonProperty("temp")]
+        public double Temperature { get ; set; }
+
+        //public double TemperatureDegres { return ((5.0 / 9.0) * (this.Temperature() - 32.) ;} 
+
+        [JsonProperty("humidity")]
+        public long Humidity { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("main")]
+        public string Visibility { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { set; get; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+    }
+}
