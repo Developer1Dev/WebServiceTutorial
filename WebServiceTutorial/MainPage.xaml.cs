3857f1ae04da5b23a2701f4116a5e8048f8f2ef6
coding-sys <56183572+coding-sys@users.noreply.github.com>
2020-06-09 22:48:01 +0200

    22h47 précisions

--- WebServiceTutorial/MainPage.xaml.cs
+++ WebServiceTutorial/MainPage.xaml.cs
@@ -5,14 +5,20 @@ namespace WebServiceTutorial
 {
     public partial class MainPage : ContentPage
     {
+        //A ContentPage is a visual element 
+        //that displays a single view and occupies most of the screen.
         RestService _restService;
 
         public MainPage()
         {
+            //Initialisation de la clase ContentPage
+            //widely observable as part of the infrastructure provided by frameworks 
+            //or technologies that use XAML combined with application
             InitializeComponent();
             _restService = new RestService();
         }
 
+        // gestion des événements avec la classe EventArgs
         async void OnButtonClicked(object sender, EventArgs e)
         {
             // On test que la variable que l'on a mise dans villeEntree n'est pas null ou un blanc
@@ -27,9 +33,9 @@ namespace WebServiceTutorial
         {
             string requestUri = endpoint;
             requestUri += $"?q={cityEntry.Text}";
-            requestUri += "&units=metric"; // or units=imperial
+            requestUri += "&units=metric"; // ou units=imperial
             requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
-            Console.WriteLine("juste pour faire un test : " + requestUri + "\n");
+            //Console.WriteLine("juste pour faire un test : " + requestUri + "\n");
             return requestUri;
         }
     }
